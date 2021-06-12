cdc72ea616fad9b743630bef6b9d205894fa043e
duoergun0130 <yanjunji0130@gmail.com>
2021-06-12 15:17:21 +0800

    0612

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mainsite.models import Post
 
-# Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', locals())
